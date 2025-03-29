1a9a24b9582ef1351c404ad871d11c6789a204f0
Adam Huffman <coronablue3@gmail.com>
2025-03-29 15:40:43 -0500

    Handle errors from the API.

--- github-activity.py
+++ github-activity.py
@@ -1,5 +1,6 @@
 import sys
 import json
+import urllib.error
 import urllib.request
 from collections import defaultdict
 
@@ -11,29 +12,32 @@ def fetch_user_events(username):
         # Make a request to the GitHub API
         events_url = f'https://api.github.com/users/{username}/events'
 
-        with urllib.request.urlopen(events_url) as response:
-            data = response.read().decode('utf-8')
-            events_list = json.loads(data)
+        try:
+            with urllib.request.urlopen(events_url) as response:
+                data = response.read().decode('utf-8')
+                events_list = json.loads(data)
+        except urllib.error.HTTPError as e:
+            events_list = {"status": e.code}
 
     return events_list
 
 def parse_events(events_list):
-    event_counts = defaultdict(int)
+    # A GitHub user may not have any events associated with them, in which case the API will return an empty list
+    if len(events_list < 1):
+        parsed_events = []
+    else:
+        event_counts = defaultdict(int)
 
-    # Count the number of occurrences for each type/repo pairing
-    # Example, if the user pushed to demo-repo 7 times, the result will be {('PushEvent', 'demo-repo'): 7}
-    for event in events_list:
-        combo_key = (event.get("type"), event.get("repo").get("name"), event.get("payload").get("action"))
-        event_counts[combo_key] += 1
+        # Count the number of occurrences for each type/repo pairing
+        # Example, if the user pushed to demo-repo 7 times, the result will be {('PushEvent', 'demo-repo'): 7}
+        for event in events_list:
+            combo_key = (event.get("type"), event.get("repo").get("name"), event.get("payload").get("action"))
+            event_counts[combo_key] += 1
 
-    # Convert back into a list of dictionaries
-    parsed_events = [
-        {"type": event_type, "repo-name": repo_name, "action": action, "count": count} for (event_type, repo_name, action), count in event_counts.items()
-    ]
-
-    total_counts = 0
-    for (key, value) in event_counts.items():
-        total_counts += value
+        # Convert back into a list of dictionaries
+        parsed_events = [
+            {"type": event_type, "repo-name": repo_name, "action": action, "count": count} for (event_type, repo_name, action), count in event_counts.items()
+        ]
 
     return parsed_events
 
@@ -142,8 +146,17 @@ if __name__ == "__main__":
     # Get user events
     events_list = fetch_user_events(username)
 
-    # Parse user events
-    parsed_events = parse_events(events_list)
+    if events_list.get("status"):
+        if events_list.get("status") == 404:
+            print("\nResource not found.")
+        elif events_list.get("status") == 403 or events_list.get("status") == 429:
+            print("\nPlease wait a few minutes, and try your request again.")
+    else:
+        # Parse user events
+        parsed_events = parse_events(events_list)
 
-    # Print events
-    print_events(parsed_events)
+        # Print events
+        if len(parse_events > 0):
+            print_events(parsed_events)
+        else:
+            print("\nNo events to display.")
